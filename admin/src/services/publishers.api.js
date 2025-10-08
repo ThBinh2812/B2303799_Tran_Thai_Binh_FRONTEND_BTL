0b557d029ac11a964821418223d9eabb3d2e3ffe
ThBinh2812 <tranthaibinh0333@gmail.com>
2025-10-08 11:37:17 +0700

    Sua loi NXB

---
+++ admin/src/services/publishers.api.js
@@ -0,0 +1,67 @@
+import axios from 'axios'
+
+const DB_URL = import.meta.env.VITE_DB_URL
+
+export const getPublishers = async () => {
+  try {
+    const res = await axios.get(`${DB_URL}/api/publishers`)
+    return res.data
+  } catch (error) {
+    return (
+      error.response?.data || {
+        status: 'error',
+        message: error.message,
+        data: null,
+      }
+    );
+  }
+};
+
+export const addPublisher = async (formData) => {
+  try {
+    const res = await axios.post(`${DB_URL}/api/publishers`, formData, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    })
+    return res.data
+  } catch (error) {
+    return (
+      error.response?.data || {
+        status: 'error',
+        message: error.message,
+        data: null,
+      }
+    );
+  }
+};
+
+export const deletePublisher = async (id) => {
+  try {
+    const res = await axios.delete(`${DB_URL}/api/publishers/${id}`);
+    return res.data;
+  } catch (error) {
+    return (
+      error.response?.data || {
+        status: 'error',
+        message: error.message,
+        data: null,
+      }
+    );
+  }
+};
+
+export const updatePublisher = async (id, formData) => {
+  try {
+    const res = await axios.put(`${DB_URL}/api/publishers/${id}`, formData, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    })
+    return res.data
+  } catch (error) {
+    return (
+      error.response?.data || {
+        status: 'error',
+        message: error.message,
+        data: null,
+      }
+    );
+  }
+};
